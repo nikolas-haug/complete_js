02f4ff3d1031a45a1cc043331fb74f61a701c4ab
Nikolas Haug <nikohaug21@gmail.com>
2019-09-17 12:14:39 -0500

    finish expert level instructor challenge

--- challenge/script.js
+++ challenge/script.js
@@ -84,6 +84,58 @@
 
 // Use IIFE to make code private
 
+// (function() {
+//   function Question(question, answers, correct) {
+//     this.question = question;
+//     this.answers = answers;
+//     this.correct = correct;
+//   }
+
+//   Question.prototype.displayQuestion = function() {
+//     console.log(this.question);
+
+//     for (var i = 0; i < this.answers.length; i++) {
+//       console.log(i + ": " + this.answers[i]);
+//     }
+//   };
+
+//   Question.prototype.checkAnswer = function(ans) {
+//     if (ans === this.correct) {
+//       console.log("Correct answer!");
+//     } else {
+//       console.log("Wrong answer, try again.");
+//     }
+//   };
+
+//   var q1 = new Question(
+//     "Is Javascript the coolest programming language in the world?",
+//     ["Yes", "No"],
+//     0
+//   );
+//   var q2 = new Question(
+//     "What is the name of this course's instructor?",
+//     ["John", "Michael", "Jonas"],
+//     2
+//   );
+//   var q3 = new Question(
+//     "What does best describe coding?",
+//     ["Boring", "Hard", "Fun", "Tedious"],
+//     2
+//   );
+
+//   var questions = [q1, q2, q3];
+
+//   var n = Math.floor(Math.random() * questions.length);
+
+//   questions[n].displayQuestion();
+
+//   var answer = parseInt(prompt("Please select the correct answer."));
+
+//   questions[n].checkAnswer(answer);
+// })();
+
+// Expert level
+
 (function() {
   function Question(question, answers, correct) {
     this.question = question;
@@ -99,13 +151,23 @@
     }
   };
 
-  Question.prototype.checkAnswer = function(ans) {
+  Question.prototype.checkAnswer = function(ans, callback) {
+    var sc;
     if (ans === this.correct) {
       console.log("Correct answer!");
+      sc = callback(true);
     } else {
       console.log("Wrong answer, try again.");
+      sc = callback(false);
     }
+    this.displayScore(sc);
   };
+
+  Question.prototype.displayScore = function(score) {
+    console.log('=============================')
+    console.log('Your current score is: ' + score);
+    console.log('=============================')
+  }
 
   var q1 = new Question(
     "Is Javascript the coolest programming language in the world?",
@@ -125,11 +187,35 @@
 
   var questions = [q1, q2, q3];
 
-  var n = Math.floor(Math.random() * questions.length);
+  // Closure ===================
+  function score() {
+    var sc = 0;
+    return function(correct) {
+      if(correct) {
+        sc++;
+      }
+      return sc;
+    }
+  }
 
-  questions[n].displayQuestion();
+  var keepScore = score();
+  //===========================
 
-  var answer = parseInt(prompt("Please select the correct answer."));
+  function nextQuestion() {
+  
+    var n = Math.floor(Math.random() * questions.length);
+  
+    questions[n].displayQuestion();
+  
+    var answer = prompt("Please select the correct answer.");
+    
+    if(answer !== 'exit') {
+      questions[n].checkAnswer(parseInt(answer), keepScore);
+      nextQuestion();
+    }
 
-  questions[n].checkAnswer(answer);
+  }
+
+  nextQuestion();
+
 })();
