dbcb2ffd24532bda4670060ac5bc7ce176113dc0
Nikolas Haug <nikohaug21@gmail.com>
2019-09-24 16:20:05 -0500

    add initial controller setup

--- simple_MVC/script.js
+++ simple_MVC/script.js
@@ -141,6 +141,12 @@ class Controller {
     constructor(model, view) {
         this.model = model;
         this.view = view;
+
+        this.onTodoListChanged(this.model.todos);
+    }
+
+    onTodoListChanged = todos => {
+        this.view.displayTodos(todos);
     }
 }
 
