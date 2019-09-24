ec1a9f1d19462a5c19ea0751190b238903137498
Nikolas Haug <nikohaug21@gmail.com>
2019-09-24 16:03:30 -0500

    add DOM elements to view

--- simple_MVC/script.js
+++ simple_MVC/script.js
@@ -35,7 +35,56 @@ class Model {
 
 class View {
     constructor() {
+        // The root element
+        this.app = this.getElement('#root');
 
+        // Title of the app
+        this.title = this.createElement('h1');
+        this.title.textContent = 'Todos';
+
+        // The form, input, and submit
+        this.form = this.createElement('form');
+
+        this.input = this.createElement('input');
+        this.input.type = 'text';
+        this.input.placeholder = 'Add todo';
+        this.input.name = 'todo';
+
+        this.submitButton = this.createElement('button');
+        this.submitButton.textContent = 'Submit';
+
+        // The visual representation of the list
+        this.todoList = this.createElement('ul', 'todo-list');
+
+        // Append the input and submit to form
+        this.form.append(this.input, this.submitButton);
+
+        // Append the title, form, and list to the app
+        this.app.append(this.title, this.form, this.todoList);
+    }
+
+    // Private methods not used outside of the class
+    get _todoText() {
+        return this.input.value;
+    }
+
+    _resetInput() {
+        this.input.value = '';
+    }
+
+    // Create an element with an optional CSS class
+    createElement(tag, className) {
+        const element = document.createElement(tag);
+        if(className) element.classList.add(className);
+
+        return element;
+    }
+
+    // Retrieve an element from the DOM
+    getElement(selector) {
+        const element = document.querySelector(selector);
+
+        return element;
     }
 }
 
