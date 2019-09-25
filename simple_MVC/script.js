3b37dc43c22b89a5ad71d8e3b224ccd080cd9c5e
Nikolas Haug <nikohaug21@gmail.com>
2019-09-25 10:20:41 -0500

    add bind methods to view

--- simple_MVC/script.js
+++ simple_MVC/script.js
@@ -135,6 +135,38 @@ class View {
             });
         }
     }
+
+    // Set up events - where 'handler' will be functions passed in
+    bindAddTodo(handler) {
+        this.form.addEventListener('submit', event => { 
+            event.preventDefault();
+
+            if(this._todoText) {
+                handler(this._todoText);
+                this._resetInput();
+            }
+        });
+    }
+
+    bindDeleteTodo(handler) {
+        this.todoList.addEventListener('click', event => {
+            if(event.target.className === 'delete') {
+                const id = parseInt(event.target.parentElement.id);
+
+                handler(id);
+            }
+        });
+    }
+
+    bindToggleTodo(handler) {
+        this.todoList.addEventListener('change', event => {
+            if(event.target.type === 'checkbox') {
+                const id = parseInt(event.target.parentElement.id);
+
+                handler(id);
+            }
+        });
+    }
 }
 
 class Controller {
@@ -147,6 +179,22 @@ class Controller {
 
     onTodoListChanged = todos => {
         this.view.displayTodos(todos);
+    }
+
+    handleAddTodo = todoText => {
+        this.model.addTodo(todoText);
+    }
+
+    handleEditTodo = (id, todoText) => {
+        this.model.editTodo(id, todoText);
+    }
+
+    handleDeleteTodo = id => {
+        this.model.deleteTodo(id);
+    }
+
+    handleToggleTodo = id => {
+        this.model.toggleTodo(id);
     }
 }
 
