f1b858558b0fcd8288fe5f5b5a0ec54637683c63
Nikolas Haug <nikohaug21@gmail.com>
2019-10-04 15:18:20 -0500

    add axios api call

--- forkApp/src/js/index.js
+++ forkApp/src/js/index.js
@@ -1,5 +1,17 @@
-// Global app controller
-import num from './test'
-const x = 23;
+import axios from 'axios';
 
-console.log(`I imported ${num} from another module, test.js! Variable x is ${x}`);
+// 2b450b0055bc32d46c596f3041d92f76
+// https://www.food2fork.com/api/search
+
+async function getResults(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '2b450b0055bc32d46c596f3041d92f76';
+    try {
+        const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        alert(error);
+    }
+}
+getResults('pizza');
